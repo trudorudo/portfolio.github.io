a7dcba55cc296a4f060c33ebf4926b54816416f7
Kristina <k.trudorudo@gmail.com>
2019-08-12 14:36:40 -0400

    changes

--- test.js
+++ test.js
@@ -1,19 +1,38 @@
-$(document).ready(function(){
-   
-    $('.parallax').parallax();
-    $('.carousel').carousel()
+$(document).ready(function () {
 
-
-    $('.carousel').carousel({
+  $('.parallax').parallax();
+     
+  $('.carousel').carousel(
+    {
+      dist: 0,
+      padding: 0,
       fullWidth: true,
       indicators: true,
-      
-    }).height(300);
-        
-  });
+      duration: 100,
+    }
+  );
+  // autoplay();
+  // function autoplay() {
+  //   $('.carousel').carousel('next');
+  //   setTimeout(autoplay, 4500);
+  // }
 
-  {
-    
+
+  //typing text
+  var i = 0;
+  var txt = " Kristina Trudorudo | Full Stack Developer |"
+  var speed = 80
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("typing-name").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
   }
-  
-  
+  typeWriter();
+
+});
+
+
+
+
